661372eae406692889cd19e35a7dfa10e1bb1780
mko <code@m-ko-x.de>
2015-02-16 21:41:49 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # tweaks-osx
 Optimizations and tweaks bundled in a shell script.
+
+#About
+
+This script modifies several OSX configuration settings due to improve operating system performance, security, behaviors and more.
+
+#WARNING
+
+Although this changes will improve system stats in general, you should consider your system admins/devops before using it, at least if you're not an IT professional.
+
+#Selection of changes
+- Removes several superfluous delays
+- Display improvements like HiDPI or subpixel rendering
+- Multiple Safari tweaks
+- Modding Mail, Terminal, Messages, Chrome and Transmission as well
+- Hibernation/Sleep acceleration 
+- and many more
+
+#More
+
+Read the script for more infos.
---
+++ osx_tweaks.sh
@@ -0,0 +1,439 @@
+#!/bin/sh
+
+#Script to tweek the mac a little
+# Do not use without reading before
+# Some things taken from here
+# https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+
+paintLine(){
+  echo ""
+  echo "#---------------------------------------------------------------------#"
+  echo ""
+}
+
+waitDefault(){
+  echo "# Tiny break to let system work for 5s"
+  sleep 5
+}
+
+echo "Welcome to the mko osx optimization and customization script"
+echo ""
+
+# Set the colours you can use
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+
+#  Reset text attributes to normal + without clearing screen.
+alias Reset="tput sgr0"
+
+# Color-echo.
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo "${2}${1}"
+  Reset # Reset to normal.
+  return
+}
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo ""
+paintLine
+cecho "#   osx tweeking script" $cyan
+cecho "#    combination of several collected performance and usability tweeks by mko..." $cyan
+paintLine
+echo ""
+
+waitDefault
+
+###############################################################################
+# General UI/UX
+###############################################################################
+
+echo ""
+echo "Name your machine?"
+read COMPUTER_NAME
+sudo scutil --set ComputerName $COMPUTER_NAME
+sudo scutil --set HostName $COMPUTER_NAME
+sudo scutil --set LocalHostName $COMPUTER_NAME
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+
+echo ""
+echo "Disabling OS X Gate Keeper ..."
+sudo spctl --master-disable
+sudo defaults write /var/db/SystemPolicy-prefs.plist enabled -string no
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+echo ""
+echo "Increasing the window resize speed for Cocoa applications"
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+echo ""
+echo "Expanding the save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+echo ""
+echo "Disabling system-wide resume"
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+echo ""
+echo "Disabling automatic termination of inactive apps"
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+echo ""
+echo "Saving to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+echo ""
+echo "Never go into computer sleep mode"
+systemsetup -setcomputersleep Off > /dev/null
+
+echo ""
+echo "Check for software updates daily, not just once per week"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+echo ""
+echo "Remove duplicates in the 'Open With' menu"
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+echo ""
+echo "Disable smart quotes and smart dashes as they're annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+paintLine
+waitDefault
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input
+###############################################################################
+
+echo ""
+echo "Increasing sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+echo ""
+echo "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo ""
+echo "Disabling press-and-hold for keys in favor of a key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+echo ""
+echo "Setting a blazingly fast keyboard repeat rate (ain't nobody got time fo special chars while coding!)"
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+echo ""
+echo "Disabling auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+echo ""
+echo "Setting trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+echo ""
+echo "Turn off keyboard illumination when computer is not used for 5 minutes"
+defaults write com.apple.BezelServices kDimTime -int 300
+
+paintLine
+waitDefault
+
+###############################################################################
+# Screen
+###############################################################################
+
+echo ""
+echo "Requiring password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+echo ""
+echo "Storing screenshots in ~/Pictures/shots by default"
+mkdir -p ~/Pictures/shots
+defaults write com.apple.screencapture location -string "$HOME/Pictures/shots"
+
+echo ""
+echo "Screenshot format PNG"
+defaults write com.apple.screencapture type -string "png"
+
+echo ""
+echo "Enabling subpixel font rendering on non-Apple LCDs"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+echo ""
+echo "Enabling HiDPI display modes (requires restart)"
+sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+paintLine
+waitDefault
+
+###############################################################################
+# Finder
+###############################################################################
+
+echo ""
+echo "Show hidden files in Finder by default"
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+
+echo ""
+echo "Show dotfiles in Finder by default"
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+echo ""
+echo "Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo ""
+echo "Showing status bar in Finder by default"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo ""
+echo "Allowing text selection in Quick Look/Preview in Finder by default"
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+echo ""
+echo "Displaying full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo ""
+echo "Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo ""
+echo "Use column view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+echo ""
+echo "Avoiding the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+echo ""
+echo "Disabling disk image verification"
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+echo ""
+echo "Enabling snap-to-grid for icons on the desktop and in other icon views"
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+paintLine
+waitDefault
+
+###############################################################################
+# Dock & Mission Control
+###############################################################################
+
+echo ""
+echo "Setting the icon size of Dock items to 40 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 40
+
+echo ""
+echo "Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock "expose-group-by-app" -bool true
+
+echo ""
+echo "Setting Dock to auto-hide and removing the auto-hiding delay"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+paintLine
+waitDefault
+
+###############################################################################
+# Safari & WebKit
+###############################################################################
+
+echo ""
+echo "Disabling Safari's thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+echo ""
+echo "Enabling Safari's debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+echo ""
+echo "Making Safari's search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+echo ""
+echo "Removing useless icons from Safari's bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+echo ""
+echo "Allow hitting the Backspace key to go to the previous page in history"
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+echo ""
+echo "Enabling the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+echo ""
+echo "Adding a context menu item for showing the Web Inspector in web views"
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+paintLine
+waitDefault
+
+###############################################################################
+# Mail
+###############################################################################
+
+echo ""
+echo "Setting email addresses to copy as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app"
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+paintLine
+waitDefault
+
+###############################################################################
+# Terminal
+###############################################################################
+
+echo ""
+echo "Enabling UTF-8 ONLY in Terminal.app"
+defaults write com.apple.terminal StringEncodings -array 4
+# to change the default theme and setting the Pro theme by default
+#defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+#defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+
+paintLine
+waitDefault
+
+###############################################################################
+# Messages                                                                    #
+###############################################################################
+
+echo ""
+echo "Disable automatic emoji substitution (i.e. use plain text smileys)"
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
+
+echo ""
+echo "Disable smart quotes as it's annoying for messages that contain code"
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+
+echo ""
+echo "Disable continuous spell checking"
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+
+paintLine
+waitDefault
+
+###############################################################################
+# Transmission.app                                                            #
+###############################################################################
+
+echo ""
+echo "Use `~/Downloads/processing` to store incomplete downloads"
+mkdir -p ~/Downloads/processing
+defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/processing"
+
+echo ""
+echo "Don't prompt for confirmation before downloading"
+defaults write org.m0k.transmission DownloadAsk -bool false
+
+echo ""
+echo "Trash original torrent files"
+defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
+echo ""
+echo "Hide the donate message"
+defaults write org.m0k.transmission WarningDonate -bool false
+
+echo ""
+echo "Hide the legal disclaimer"
+defaults write org.m0k.transmission WarningLegal -bool false
+
+paintLine
+waitDefault
+
+###############################################################################
+# Personal Additions
+###############################################################################
+
+echo ""
+echo "Disable hibernation (speeds up entering sleep mode)"
+# data will be kept in RAM and not saved to HDD
+sudo pmset -a hibernatemode 0
+
+echo ""
+echo "Remove the sleep image file to save disk space"
+sudo rm -rf /Private/var/vm/sleepimage
+echo "creating a zero-byte file instead"
+sudo touch /Private/var/vm/sleepimage
+echo "and make sure it can't be rewritten"
+sudo chflags uchg /Private/var/vm/sleepimage
+
+echo ""
+echo "Disable the sudden motion sensor as it's not useful for SSDs"
+sudo pmset -a sms 0
+
+echo ""
+echo "Speeding up wake from sleep to 24 hours from an hour"
+# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
+sudo pmset -a standbydelay 86400
+
+echo ""
+echo "Disable annoying backswipe in Chrome"
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+
+paintLine
+waitDefault
+
+###############################################################################
+# Kill affected applications
+###############################################################################
+
+echo ""
+cecho "Done!" $white
+echo ""
+echo ""
+cecho "###############################################################################" $yellow
+echo ""
+echo ""
+cecho "Some of these changes require a logout/restart to take effect." $white
+cecho "Killing some open applications in order to take effect. Restarting several apps and exiting. Finally providing restart." $white
+echo ""
+
+echo "Restarting now is recommended. At least killing severall apps now is necessary. Press 'R' for a restart, 'K' for just killing apps or any other key for doing nothing."
+read vLeFin
+
+if [ "$LeFin" = r ]; then
+  
+  sudo shutdown -r now
+  
+elif [ "$LeFin" = k ]; then
+  
+  find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
+  for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+    "Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
+    "Terminal" "Transmission"; do
+    killall "${app}" > /dev/null 2>&1
+
+  done
+  
+fi
+
+echo "Exiting without restart of system/applications (NOT RECOMMENDED)"
+exit 0
